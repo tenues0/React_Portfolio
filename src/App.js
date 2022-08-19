cf85e03a457b22b6e9e4f8f5ea2a9d1cb744c046
tenues0 <theodore.ayoub@gmail.com>
2022-08-19 10:37:59 -0700

    added Nav to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Nav from './components/Nav';
 import HelloReact from './components/HelloReact';
 
 // TODO: Add a comment explaining what this function is doing
 // This function is a functional component that helps us split the UI into distinct parts.
-// In this case, we are returning another component, <HelloReact/> from it.
-function App() {
-  return <HelloReact />;
+// In this case, we are returning other components, <Nav /> and <HelloReact/> from it.
+
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <HelloReact />
+    </div>
+  );
 }
 
-export default App;
+// export default App;
