fb4c28646a39a7c32add3d915e7c619e9c065f72
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-08-19 19:17:13 -0700

    Updates

---
+++ precache-manifest.14adce52b7cba5ede4ae1fa2a407e2c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "738be5dce27d02ca3350",
+    "url": "/React_Portfolio/static/css/main.d65ba7a5.chunk.css"
+  },
+  {
+    "revision": "738be5dce27d02ca3350",
+    "url": "/React_Portfolio/static/js/main.738be5dc.chunk.js"
+  },
+  {
+    "revision": "3ab6507db92d02096f61",
+    "url": "/React_Portfolio/static/css/1.5314e4b7.chunk.css"
+  },
+  {
+    "revision": "3ab6507db92d02096f61",
+    "url": "/React_Portfolio/static/js/1.3ab6507d.chunk.js"
+  },
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "e56ae807fa79b0454d3c8d5da6e69c1f",
+    "url": "/React_Portfolio/static/media/Teddy.e56ae807.jpg"
+  },
+  {
+    "revision": "97b3c7dea6a3af52c9f4526b40e509d8",
+    "url": "/React_Portfolio/static/media/kb.97b3c7de.jpg"
+  },
+  {
+    "revision": "80981e5d6565a134d2e42a0aafc05ee9",
+    "url": "/React_Portfolio/index.html"
+  }
+];
